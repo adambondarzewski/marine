397001e7293208d2af1f387b7a633f9203d3c2da
Adam Bond <adam.bondarzewski@contractors.roche.com>
2021-01-14 22:56:32 +0100

    stuff

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -8,27 +8,21 @@
 #
 
 library(shiny)
-library(Marine)
+library(semantic.dashboard)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+shinyUI(dashboardPage(
+    dashboardHeader(title = "Basic dashboard"),
+    dashboardSidebar(sidebarMenu(
+        menuItem(tabName = "home", text = "Home", icon = icon("home")),
+        menuItem(tabName = "another", text = "Another Tab", icon = icon("heart"))
+    )),
+    dashboardBody(
+        fluidRow(
+            box(plotOutput("plot1", height = 250)),
+            box(
+                title = "Controls",
+                sliderInput("slider", "Number of observations:", 1, 100, 50)
+            )
         )
-    )
-))
+    )))
