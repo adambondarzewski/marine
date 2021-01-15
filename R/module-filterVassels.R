2dd5ef9a63c3678d24491db10404e8de0207cebc
Adam Bond <adam.bondarzewski@contractors.roche.com>
2021-01-15 21:41:34 +0100

    module skeleton

---
+++ R/module-filterVassels.R
@@ -0,0 +1,13 @@
+filterVasselsInput <- function(id) {
+  ns <- NS(id)
+}
+
+# Module server function
+filterVasselsServer <- function(id) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+
+    }
+  )
+}
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -2,18 +2,18 @@ library(shiny)
 library(semantic.dashboard)
 
 # Define UI for application that draws a histogram
-shinyUI(dashboardPage(
-    dashboardHeader(title = "Basic dashboard"),
-    dashboardSidebar(sidebarMenu(
-        menuItem(tabName = "home", text = "Home", icon = icon("home")),
-        menuItem(tabName = "another", text = "Another Tab", icon = icon("heart"))
-    )),
-    dashboardBody(
-        fluidRow(
-            box(plotOutput("plot1", height = 250)),
-            box(
-                title = "Controls",
-                sliderInput("slider", "Number of observations:", 1, 100, 50)
+    shinyUI(dashboardPage(
+        dashboardHeader(title = "Basic dashboard"),
+        dashboardSidebar(sidebarMenu(
+            menuItem(tabName = "home", text = "Home", icon = icon("home")),
+            menuItem(tabName = "another", text = "Another Tab", icon = icon("heart"))
+        )),
+        dashboardBody(
+            fluidRow(
+                box(plotOutput("plot1", height = 250)),
+                box(
+                    title = "Controls",
+                    sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
             )
-        )
-    )))
+        )))
